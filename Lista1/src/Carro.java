e8309c22ee37344bd0b470242d4d773df3682c20
viniciuslprado <viniciusdelucaprado@gmail.com>
2025-02-24 21:46:28 -0300

    meu primeiro programa em java

---
+++ Lista1/src/Carro.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+
+public class Carro {
+    //variaveis
+    //do tipo primitivo (começa com letra minuscula)
+    public int ano;
+    public float velocidade;
+    //do tipo classe
+    public String marca, modelo;
+
+    //todo metodo constrtutor é o mesmo nome da classe
+    //metodo construtor com parametro
+    public Carro() {
+        this.marca = "Marca inválida";
+        this.modelo = "Modelo Inválido";
+    }
+
+    //metodo construtor sem parametro
+    public Carro(int ano, int velocidade, String marca, String modelo) {
+        this.ano = ano;
+        this.velocidade = velocidade;
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+    //metodos
+    public void exibirDetalhes() {
+        //System.out.println("Marca: " + this.marca + " Modelo:  " + this.modelo + " Ano: " + this.ano + " Velocidade: " + this.velocidade);
+        JOptionPane.showMessageDialog(null, "Marca: " + this.marca + " Modelo:  " + this.modelo + " Ano: " + this.ano + " Velocidade: " + this.velocidade);
+    }
+
+    // vai acelerar o carro de X unidade
+    public void acelerar(float x) {
+        this.velocidade = this.velocidade + x;
+    }
+
+    public void frear(float x) {
+        if(this.velocidade - x >= 0){
+            this.velocidade = this.velocidade - x;
+        }
+        else{
+            System.out.println("Não é possivel frear");
+        }
+    }
+}
---
+++ Lista1/src/Main.java
@@ -0,0 +1,29 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    /*public static void main(String[] args) {
+        // criando objetos da classe
+        // Instanciar a classe, e criando objetos
+        Carro obj1 = new Carro();
+        obj1.ano = 2022;
+        obj1.velocidade = 0;
+        obj1.marca = "Volkswagen";
+        obj1.modelo = "Polo";
+        obj1.exibirDetalhes();
+        obj1.acelerar(100);
+        obj1.exibirDetalhes();
+        obj1.frear(20);
+        obj1.exibirDetalhes();
+
+        Carro obj2 = new Carro(2024,0,"Fiat", "FastBack");
+        obj2.exibirDetalhes();
+        obj2.acelerar(30);
+        obj2.exibirDetalhes();
+        obj2.frear(40);
+        obj2.exibirDetalhes();
+
+        Carro obj3 = new Carro();
+        obj3.exibirDetalhes();
+
+    }*/
+}
